ba95d09509e58852bbbcb6d69abe9654283f6096
Lucas Justino <lucaspbj@hotmail.com>
2023-09-05 22:32:31 +0100

    add initial css style and create JavaScript file

---
+++ assets/js/index.js
@@ -0,0 +1,58 @@
+// script.js
+const choices = ["rock", "paper", "scissors", "lizard", "spock"];
+let userScore = 0;
+let tries = 3;
+
+const buttons = document.querySelectorAll("#game button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", function () {
+        const userChoice = button.id;
+        const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+
+        const result = getResult(userChoice, computerChoice);
+        updateUI(result);
+
+        tries--;
+
+        if (tries === 0) {
+            endGame();
+        }
+    });
+});
+
+function getResult(user, computer) {
+    if (user === computer) return "draw";
+    if (
+        (user === "rock" && (computer === "scissors" || computer === "lizard")) ||
+        (user === "paper" && (computer === "rock" || computer === "spock")) ||
+        (user === "scissors" && (computer === "paper" || computer === "lizard")) ||
+        (user === "lizard" && (computer === "spock" || computer === "paper")) ||
+        (user === "spock" && (computer === "rock" || computer === "scissors"))
+    ) {
+        userScore++;
+        return "win";
+    }
+    return "lose";
+}
+
+function updateUI(result) {
+    document.getElementById("result").innerHTML = `
+        <p>${getResultText(result)}</p>
+        <p>Remaining tries: <span id="tries">${tries}</span></p>
+        <p>Score: <span id="score">${userScore}</span></p>
+    `;
+}
+
+function getResultText(result) {
+    if (result === "draw") return "It's a draw!";
+    if (result === "win") return "You win!";
+    return "You lose!";
+}
+
+function endGame() {
+    buttons.forEach((button) => {
+        button.removeEventListener("click", function () {});
+    });
+    document.getElementById("game").innerHTML = "<p>Game over!</p>";
+}
