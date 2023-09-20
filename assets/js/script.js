ab1c5218e9c83f204ef6e30815119a3d71c5c47d
Lucas Justino <lucaspbj@hotmail.com>
2023-09-20 22:47:11 +0100

    add better structure to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,92 +1,104 @@
+// Wait for the DOM to finish loading before running the game
 document.addEventListener("DOMContentLoaded", function () {
-    // game choices
+    // Define game choices
     const choices = ["rock", "paper", "scissors", "lizard", "spock"];
-  
-    // user points and tries
+
+    // Initialize user points and remaining tries
     let userScore = 0;
     let tries = 3;
-  
-    // select all the game buttons by class "game-button"
+
+    // Select all the game buttons by class "game-button"
     const buttons = document.querySelectorAll(".game-button");
     const refreshButton = document.querySelector(".refresh-button");
-  
+
+    // Add click event listeners to each game button
     buttons.forEach((button) => {
-      button.addEventListener("click", function () {
-        const userChoice = button.getAttribute("data-choice");
-        const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-  
-        // Atualiza as escolhas do usuário e do computador no HTML
-        document.getElementById("user-choice").textContent = `Your choice: ${userChoice}`;
-        document.getElementById("computer-choice").textContent = `Computer's choice: ${computerChoice}`;
-  
-        const result = getResult(userChoice, computerChoice);
-        updateUI(result);
-  
-        tries--;
-  
-        if (tries === 0) {
-          endGame();
-        }
-      });
+        button.addEventListener("click", function () {
+            const userChoice = button.getAttribute("data-choice");
+            const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+
+            // Update user and computer choices in the HTML
+            document.getElementById("user-choice").textContent = `Your choice: ${userChoice}`;
+            document.getElementById("computer-choice").textContent = `Computer's choice: ${computerChoice}`;
+
+            const result = getResult(userChoice, computerChoice);
+            updateUI(result);
+
+            tries--;
+
+            if (tries === 0) {
+                endGame();
+            }
+        });
     });
-  
+
+    // Determine the game result based on user and computer choices
     function getResult(user, computer) {
-      if (user === computer) return "draw";
-      if (
-        (user === "rock" && (computer === "scissors" || computer === "lizard")) ||
-        (user === "paper" && (computer === "rock" || computer === "spock")) ||
-        (user === "scissors" && (computer === "paper" || computer === "lizard")) ||
-        (user === "lizard" && (computer === "spock" || computer === "paper")) ||
-        (user === "spock" && (computer === "rock" || computer === "scissors"))
-      ) {
-        userScore++;
-        return "win";
-      }
-      return "lose";
+        if (user === computer) return "draw";
+        if (
+            (user === "rock" && (computer === "scissors" || computer === "lizard")) ||
+            (user === "paper" && (computer === "rock" || computer === "spock")) ||
+            (user === "scissors" && (computer === "paper" || computer === "lizard")) ||
+            (user === "lizard" && (computer === "spock" || computer === "paper")) ||
+            (user === "spock" && (computer === "rock" || computer === "scissors"))
+        ) {
+            userScore++;
+            return "win";
+        }
+        return "lose";
     }
-  
+
+    // Update the UI with the game result and remaining tries
     function updateUI(result) {
-      document.querySelector("#result p").textContent = getResultText(result);
-      document.querySelector("#tries").textContent = tries;
-      document.querySelector("#score").textContent = userScore;
+        document.querySelector("#result p").textContent = getResultText(result);
+        document.querySelector("#tries").textContent = tries;
+        document.querySelector("#score").textContent = userScore;
     }
-  
+
+    // Get the text representation of the game result
     function getResultText(result) {
-      if (result === "draw") return "It's a draw!";
-      if (result === "win") return "You win!";
-      return "You lose!";
+        if (result === "draw") return "It's a draw!";
+        if (result === "win") return "You win!";
+        return "You lose!";
     }
-  
+
+    // End the game when tries are exhausted
     function endGame() {
-      buttons.forEach((button) => {
-        button.removeEventListener("click", function () {});
-      });
-      document.querySelector(".game").innerHTML = "<p>Game over!</p>";
-  
-      // Mostra o botão "New Game" quando o jogo termina
-      refreshButton.style.display = "block";
+        // Remove click event listeners from game buttons
+        buttons.forEach((button) => {
+            button.removeEventListener("click", function () { });
+        });
+        // Display "Game over!" message in the game container
+        document.querySelector(".game").innerHTML = "<p>Game over!</p>";
+
+        // Show the "New Game" button when the game ends
+        refreshButton.style.display = "block";
     }
-  
-    // button for new game
+
+    // Add click event listener to the "New Game" button
     refreshButton.addEventListener("click", function () {
-      location.reload();
+        // Reload the page to start a new game
+        location.reload();
     });
-  });
-  
-  // No seu código JavaScript existente...
+});
+
+// Additional code outside of the main game logic
+
+// Show instructions popup when the instruction button is clicked
 const instructionButton = document.querySelector(".instruction-button");
 const popup = document.querySelector(".popup");
 const closeButton = document.querySelector(".close-button");
 
 instructionButton.addEventListener("click", () => {
-    popup.style.display = "block"; // Mostra a janela modal ao clicar no botão de instruções
+    popup.style.display = "block";
 });
 
+// Close the instructions popup when the close button is clicked
 closeButton.addEventListener("click", () => {
-    popup.style.display = "none"; // Fecha a janela modal ao clicar no botão de fechar
+    popup.style.display = "none";
 });
 
-// Fechar a janela modal ao clicar fora dela
+// Close the instructions popup when clicking outside of it
 window.addEventListener("click", (event) => {
     if (event.target === popup) {
         popup.style.display = "none";
